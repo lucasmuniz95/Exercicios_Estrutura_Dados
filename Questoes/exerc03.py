8726fab7eae7a1bbd32cf61a9a85a1d9c2fe2d07
lucasmuniz95 <100134869+lucasmuniz95@users.noreply.github.com>
2022-09-20 10:14:41 -0300

    Create exerc03.py

---
+++ Questoes/exerc03.py
@@ -0,0 +1,6 @@
+
+temperatura = int(input('Digite a temperatura em graus Celsius: '))
+
+fahrenheit = (temperatura * 9 / 5) + 32
+
+print(f'A temperatura de {temperatura} °C corresponde a {fahrenheit} °F')
